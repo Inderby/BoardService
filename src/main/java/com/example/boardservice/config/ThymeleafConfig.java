48adca0f7abbd5abf6de998cdb5b8eba8e5bf306
Inderby <youk8253@gmail.com>
2024-01-28 22:34:53 +0900

    #16 - Thymeleaf의 decoupled logic 설정 수동처리
    devtools를 사용한 live reload 기능을 사용하니 bean intialize 순서 때문인지, 아니면 properties binding 인식 오류인지 모르겠으니 `Thymeleaf3Properites`의 configuration 값이 제대로 인식되지 않는 오류가 발생하여 수동으로 decoupled logic을 true로 변경시켜주도록 변경
    
    dev tools의 기능을 실행시키고 cache 기능을 껐다

--- src/main/java/com/example/boardservice/config/ThymeleafConfig.java
+++ src/main/java/com/example/boardservice/config/ThymeleafConfig.java
@@ -1,36 +1,35 @@
 package com.example.boardservice.config;
 
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
+import lombok.Setter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 
 @Configuration
+//@EnableConfigurationProperties(ThymeleafConfig.Thymeleaf3Properties.class)
 public class ThymeleafConfig {
     @Bean
     public SpringResourceTemplateResolver thymeleafTemplateResolver(
-            SpringResourceTemplateResolver defaultTemplateResolver,
-            Thymeleaf3Properties thymeleaf3Properties
+            SpringResourceTemplateResolver defaultTemplateResolver
     ) {
-        defaultTemplateResolver.setUseDecoupledLogic(thymeleaf3Properties.isDecoupledLogic());
+        defaultTemplateResolver.setUseDecoupledLogic(true);
         return defaultTemplateResolver;
     }
 
-    @Getter
-    @ConfigurationProperties("spring.thymeleaf3")
-    public static class Thymeleaf3Properties {
-        private final boolean decoupledLogic;
 
-        @ConstructorBinding
+    @Getter
+    @Setter
+    public static class Thymeleaf3Properties {
+        private boolean decoupledLogic;
+
+//        @ConstructorBinding
         public Thymeleaf3Properties(boolean decoupledLogic) {
             /**
              * Use Thymeleaf 3 Decoupled Logic
              * */
             this.decoupledLogic = decoupledLogic;
         }
+
     }
 }
