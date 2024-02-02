233f8195300832506d8a51dfee51016bc3164e81
Inderby <youk8253@gmail.com>
2024-02-02 21:41:48 +0900

    #32 - 검색 기능을 위해 SearchType 옵션 전달, 테스트
    게시판 페이지의 검백 종류를 드롭 다운 메뉴로 제공할 때, 해당 값을 뷰가 알게 하지 않고 서버로부터 받아서 쓰게 만들기 위해 값을 전달하는 로직을 테스트와 함께 추가

--- src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
+++ src/test/java/com/example/boardservice/controller/ArticleControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.boardservice.controller;
 
 import com.example.boardservice.config.SecurityConfig;
+import com.example.boardservice.domain.type.SearchType;
 import com.example.boardservice.service.ArticleService;
 import com.example.boardservice.service.PaginationService;
 import com.example.boardservice.service.TestFixture;
@@ -61,6 +62,30 @@ class ArticleControllerTest {
         then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
     }
 
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 검색어와 함께 호출")
+    @Test
+    public void givenKeyword_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
+        //Given
+        SearchType searchType = SearchType.TITLE;
+        String searchValue = "title";
+        given(articleService.searchArticles(eq(searchType), eq(searchValue), any(Pageable.class))).willReturn(Page.empty()); //필드중 일부만 matcher를 사용할 수 없기 때문에 null에 대해서도 wrapping 해줌
+        given(paginationService.getPaginationBarNumbers(anyInt(), anyInt())).willReturn(List.of(0, 1, 2, 3, 4));
+        //When&Then
+        mvc.perform(get("/articles")
+                        .queryParam("searchType", searchType.name())
+                        .queryParam("searchValue", searchValue)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))//호환되는 유형까지 맞다고 처리해줌
+                .andExpect(view().name("articles/index")) //view 파일의 이름을 조건에 추가해줄 수 있다
+                .andExpect(model().attributeExists("articles")) //서버에서 view로 밀어넣어준 model 중에 articles 속성이 있는지 확인
+                .andExpect(model().attributeExists("paginationBarNumbers"))
+                .andExpect(model().attributeExists("searchTypes"))
+        ;
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(paginationService).should().getPaginationBarNumbers(anyInt(), anyInt());
+    }
+
     @DisplayName("[view][GET] 게시글 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
@@ -84,7 +109,7 @@ class ArticleControllerTest {
 
     @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 기능")
     @Test
-    void givenPagingAndSortingParams_whenSearchingArticlesPage_thenReturnsArticlesPage() throws Exception {
+    void givenPagingAndSortingParams_whenSearchingArticlesView_thenReturnsArticlesView() throws Exception {
         // Given
         String sortName = "title";
         String direction = "desc";
