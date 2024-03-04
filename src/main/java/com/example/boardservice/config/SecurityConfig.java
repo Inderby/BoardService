85513e15ff560947710f421b92075e19b7dad810
Inderby <youk8253@gmail.com>
2024-03-04 14:00:36 +0900

    #87 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB인증에 더해 OAuth 인증까지 받아들일 수 있도록 설정 추가
    
    해당 변경을 테스트에도 반영

--- src/main/java/com/example/boardservice/config/SecurityConfig.java
+++ src/main/java/com/example/boardservice/config/SecurityConfig.java
@@ -1,37 +1,80 @@
 package com.example.boardservice.config;
 
-import com.example.boardservice.dto.UserAccountDto;
 import com.example.boardservice.dto.security.BoardPrincipal;
-import com.example.boardservice.repository.UserAccountRepository;
+import com.example.boardservice.dto.security.KakaoOAuth2Response;
+import com.example.boardservice.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         http
                 .authorizeHttpRequests(auth-> auth
-                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll() //정적 자원에 대한 보안 요소를 Spring Security 관리하에 두도록 한다.
-                        .requestMatchers(HttpMethod.GET,
+                        .requestMatchers( PathRequest.toStaticResources().atCommonLocations()).permitAll() //정적 자원에 대한 보안 요소를 Spring Security 관리하에 두도록 한다.
+                        .requestMatchers( HttpMethod.GET,
                                 "/"
                                 , "/articles"
                                 ,"/articles/search-hashtag", "/error").permitAll()
                         .anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults())
-                .logout(logout -> logout.logoutSuccessUrl("/"));
-
+                .formLogin(withDefaults())
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(oAuth -> oAuth
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(oAuth2UserService)
+                        )
+                );
         return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService,
+            PasswordEncoder passwordEncoder
+    ){
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoOAuth2Response = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); //예상 값 : "kakao"
+            String providerId = String.valueOf(kakaoOAuth2Response.id());
+            String username = registrationId + "_" + providerId;
+            String dummyPassword = passwordEncoder().encode("{bcrypt}" + UUID.randomUUID());
+            return userAccountService.searchUser(username)
+                    .map(BoardPrincipal::from)
+                    .orElseGet(()->
+                            BoardPrincipal.from(
+                                    userAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            kakaoOAuth2Response.email(),
+                                            kakaoOAuth2Response.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
     }
 
     //TODO : 예전에 사용되던 방식 지금은 httpSecurity
@@ -42,10 +85,9 @@ public class SecurityConfig {
 //    }
 
     @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository){
-        return (username -> userAccountRepository
-                .findById(username)
-                .map(UserAccountDto::from)
+    public UserDetailsService userDetailsService(UserAccountService userAccountService){
+        return (username -> userAccountService
+                .searchUser(username)
                 .map(BoardPrincipal::from)
                 .orElseThrow( () -> new UsernameNotFoundException("유저를 찾을 수 없습니다"))
                 );
--- src/test/java/com/example/boardservice/config/TestSecurityConfig.java
+++ src/test/java/com/example/boardservice/config/TestSecurityConfig.java
@@ -1,7 +1,9 @@
 package com.example.boardservice.config;
 
 import com.example.boardservice.domain.UserAccount;
+import com.example.boardservice.dto.UserAccountDto;
 import com.example.boardservice.repository.UserAccountRepository;
+import com.example.boardservice.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
@@ -12,15 +14,23 @@ import static org.mockito.BDDMockito.*;
 @Import(SecurityConfig.class)
 public class TestSecurityConfig { //컨트롤러 테스트와 관련하여 UserAccount repository의 인증 정보를 자동으로 추가해줄 수 있도록 도와주는 설정
     @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod //테스트 코드에 한정해서 인증 정보를 넣어줄 수 있게 해주는 어노테이샨
     public void securitySetUp(){
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(),anyString(),anyString(),anyString(),anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
                 "inderby",
                 "pw",
                 "test@email.com",
                 "test",
                 "test memo"
-        )));
+        );
     }
 }
