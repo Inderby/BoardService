cb0e9aa9fca2d4669fd9a2c600e1507cdeae76b3
Inderby <youk8253@gmail.com>
2024-03-27 23:29:16 +0900

    #109 - 빠트린 어노테이션 추가

--- src/main/java/com/example/boardservice/repository/HashtagRepository.java
+++ src/main/java/com/example/boardservice/repository/HashtagRepository.java
@@ -5,12 +5,14 @@ import com.example.boardservice.repository.querydsl.HashtagRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.core.parameters.P;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+@RepositoryRestResource
 public interface HashtagRepository extends JpaRepository<Hashtag, Long>, QuerydslPredicateExecutor<Hashtag>, HashtagRepositoryCustom {
     Optional<Hashtag> findByHashtagName(@Param("hashtagName") String hashtagName);
     List<Hashtag> findByHashtagNameIn(@Param("hashtagNames") Set<String> hashtagNames);
